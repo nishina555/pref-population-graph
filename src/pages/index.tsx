58b89aabc19e74dc7a4e3a02d6bd5eacd448c616
nishina555 <n.toshi.1.014@gmail.com>
2023-02-23 16:39:45 +0900

    env追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from "next";
 import { css } from "@emotion/react";
 
 const Home: NextPage = () => {
-  return <h1 css={helloStyle}>Hello</h1>;
+  return (
+    <div>
+      <h1 css={helloStyle}>Hello</h1>
+    </div>
+  );
 };
 
 const helloStyle = css({
