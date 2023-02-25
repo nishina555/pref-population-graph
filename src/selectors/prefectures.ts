3e69040db75f9a69eec63d96c4f023efcd7e081b
nishina555 <n.toshi.1.014@gmail.com>
2023-02-25 11:03:54 +0900

    selectorの実装

---
+++ src/selectors/prefectures.ts
@@ -0,0 +1,23 @@
+import { AppState } from "../store/index";
+import { PrefectureEntity } from "../types/state/prefectures";
+import { createSelector } from "@reduxjs/toolkit";
+import { Entities } from "../types/state/base";
+
+const selectPrefectureEntities = (
+  state: AppState
+): Entities<PrefectureEntity> => state.entities.prefectures;
+
+const selectPrefectureIds = createSelector(
+  [selectPrefectureEntities],
+  (prefectures) => prefectures.allIds
+);
+
+const selectPrefectureById = createSelector(
+  [selectPrefectureEntities],
+  (prefectures) => prefectures.byId
+);
+
+const selectPrefectures = createSelector(
+  [selectPrefectureIds, selectPrefectureById],
+  (prefectureIds, prefectures) => prefectureIds.map((id) => prefectures[id])
+);
