78825ec1eaf331798a6c0eaac15621e51327f4db
nishina555 <n.toshi.1.014@gmail.com>
2023-02-25 10:55:27 +0900

    サンプルデータをinitialStateにセット

--- src/reducers/prefecturesSlice.ts
+++ src/reducers/prefecturesSlice.ts
@@ -1,8 +1,19 @@
+import { Entities } from "@/types/state/base";
+import { PrefectureEntity } from "@/types/state/prefectures";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  allIds: [],
-  byId: {},
+const initialState: Entities<PrefectureEntity> = {
+  allIds: [1, 2],
+  byId: {
+    1: {
+      prefCode: 1,
+      prefName: "北海道",
+    },
+    2: {
+      prefCode: 2,
+      prefName: "青森",
+    },
+  },
 };
 
 const prefecturesSlice = createSlice({
