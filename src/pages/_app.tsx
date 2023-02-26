8d6c3cba5f02649b9dbd295dc6908850ceba9e54
nishina555 <n.toshi.1.014@gmail.com>
2023-02-26 19:29:01 +0900

    add ress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../store/index";
+import "ress";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
