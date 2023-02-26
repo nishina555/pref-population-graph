e1adb2dba4d6b53498a4a87262da858ae8b882c7
nishina555 <n.toshi.1.014@gmail.com>
2023-02-26 19:56:43 +0900

    不要なコンポーネント削除

--- src/components/homePage/PopulationViewContainer.tsx
+++ src/components/homePage/PopulationViewContainer.tsx
@@ -5,9 +5,10 @@ import {
   PopulationHistoryEntity,
 } from "@/types/state/populationHistories";
 import { PrefectureEntity } from "@/types/state/prefectures";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import { FC } from "react";
 import { useSelector } from "react-redux";
-import { PopulationView } from "./PopulationView";
 
 export const PopulationViewContainer: FC = () => {
   const populations: PopulationHistoryEntity[] = useSelector(selectPopulations);
@@ -24,5 +25,34 @@ export const PopulationViewContainer: FC = () => {
     return { name: prefecture[0]["prefName"], data };
   });
 
-  return <PopulationView plotData={plotData} />;
+  const options: Highcharts.Options = {
+    title: {
+      text: "",
+    },
+    yAxis: {
+      title: {
+        text: "人口数",
+      },
+    },
+    xAxis: {
+      min: 1960,
+      max: 2045,
+      tickInterval: 5,
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 1960,
+      },
+    },
+  };
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={{ ...options, series: plotData }}
+    />
+  );
 };
