4005ff78f9562e462dcd48380d3fbfa40080f292
nishina555 <n.toshi.1.014@gmail.com>
2023-03-01 22:01:37 +0900

    ローディングコンポーネントの実装

---
+++ src/components/shared/Loading.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { css } from "@emotion/react";
+
+export const Loading: FC = () => {
+  return (
+    <div css={container}>
+      <img css={icon} src="images/loading.svg" />
+    </div>
+  );
+};
+
+const container = css({
+  marginTop: "1rem",
+});
+
+const icon = css({
+  width: "40px",
+  height: "40px",
+});
