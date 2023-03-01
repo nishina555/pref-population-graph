eb76b8face472987b3b9bce9f0ddc644a4c0c0e0
nishina555 <n.toshi.1.014@gmail.com>
2023-03-01 21:34:00 +0900

    update requestsSlice extraReducers

--- src/reducers/requestsSlice.ts
+++ src/reducers/requestsSlice.ts
@@ -1,11 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { RequestStatus } from "@/types/constants/requestStatusType";
+import { RequestState } from "@/types/state/requests";
+import {
+  ActionReducerMapBuilder,
+  AsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { getPrefectures } from "./prefecturesSlice";
 
 const initialState = {};
+
+const requestAsyncThunks = [getPrefectures];
+
+const createRequestAsyncThunkReducer = (
+  builder: ActionReducerMapBuilder<RequestState>,
+  targetRequestThunk: AsyncThunk<any, any, any>
+) => {
+  builder.addMatcher(
+    (action) => action.type.startsWith(`${targetRequestThunk.typePrefix}/`),
+    (state, action) => {
+      switch (action.meta.requestStatus) {
+        case "pending": {
+          state[targetRequestThunk.typePrefix] = {
+            status: RequestStatus.Request,
+          };
+          break;
+        }
+        case "fulfilled": {
+          state[targetRequestThunk.typePrefix] = {
+            status: RequestStatus.Success,
+          };
+          break;
+        }
+        case "rejected": {
+          state[targetRequestThunk.typePrefix] = {
+            status: RequestStatus.Failure,
+          };
+          break;
+        }
+        default: {
+          return;
+        }
+      }
+    }
+  );
+};
 
 export const requestSlice = createSlice({
   name: "request",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    requestAsyncThunks.forEach((thunk) => {
+      createRequestAsyncThunkReducer(builder, thunk);
+    });
+  },
 });
 
 export default requestSlice.reducer;
