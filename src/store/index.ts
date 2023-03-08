5370e42a737d8deb93f8729952460fd85368395a
nishina555 <n.toshi.1.014@gmail.com>
2023-03-08 21:05:36 +0900

    add test-utils

--- src/store/index.ts
+++ src/store/index.ts
@@ -29,3 +29,5 @@ export const store = setupStore();
 export type AppState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
+
+export type AppStore = ReturnType<typeof setupStore>;
